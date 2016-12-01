e2f076a3fe0f3d1dc3208c74a09406715e4b4fbc
Matthew Chambault <matthewchambault@Matthews-MacBook-Pro.local>
2016-12-01 10:12:04 +0000

    CS346 is amazing + r and w values passed

--- v2/runServers.py
+++ v2/runServers.py
@@ -6,13 +6,15 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 n = int(sys.argv[1])
+r = int(sys.argv[2])
+w = int(sys.argv[3])
 if (n < 4 or n > 10):
     print("Please enter a value between 4 and 10")
     sys.exit(1)
 
 address = "localhost:903"
 for i in range(0, n):
-    s = "java Site " + str(i) + " 2" + " 3 " + str(n) + " "
+    s = "java Site " + str(i) + " " + str(r) + " " + str(w) + " " + str(n) + " "
     for j in range(0, n-1):
         if i != j:
             s += address+ str(j) +','
--- v2/runSimulation.sh
+++ v2/runSimulation.sh
@@ -3,10 +3,14 @@
 #The runner for our databases simulation
 
 numberOfServers=$1
+rValue=$2
+wValue=$3
 
 echo "The number of servers that you want to run is: " $numberOfServers
+echo "The R value is: " $rValue
+echo "The W value is: " $wValue
 
 ./removeTextFiles.sh
 python processTransactions.py
-python runServers.py $numberOfServers
+python runServers.py $numberOfServers $rValue $wValue
 echo "Servers started"
